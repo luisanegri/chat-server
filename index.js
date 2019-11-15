e7c98ee16d6ebbb0f279e55f728d06670e5aa60d
luisanegri <luisaanegri@gmail.com>
2019-11-15 10:05:55 +0100

    create new message endpoint

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+app.use(jsonParser);
+app.get('/', (req, res, next) => {
+  res.send('hello');
+});
+
+const messages = [];
+
+app.post('/message', (req, res, next) => {
+  // get the message out of the body of the req
+  const { message } = req.body;
+  // push it to array messages
+  messages.push(message);
+  res.send(message);
+});
 
 app.listen(port, () => console.log(`listening on port ${port}`));

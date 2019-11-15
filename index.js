add5224bec99b4f8030d2d5a057e36098b195432
luisanegri <luisaanegri@gmail.com>
2019-11-15 15:12:13 +0100

    create streams dictionary

--- index.js
+++ index.js
@@ -16,9 +16,12 @@ app.get('/', (req, res, next) => {
 
 const stream = new See();
 
+const streams = {};
+
 app.get('/stream', (req, res, next) => {
+  const rooms = Object.keys(messages);
   //serialize: turn into a string - normally used when needs to send something to the internet
-  const string = JSON.stringify(messages);
+  const string = JSON.stringify(rooms);
 
   // send the data stored in string to the client
   stream.updateInit(string);
@@ -27,20 +30,63 @@ app.get('/stream', (req, res, next) => {
   stream.init(req, res);
 });
 
-const messages = [];
+app.get('/rooms/:roomName', (req, res, next) => {
+  const { roomName } = req.params;
 
-app.post('/message', (req, res, next) => {
+  const { stream } = streams[roomName];
+
+  const data = messages[roomName];
+  //serialize: turn into a string - normally used when needs to send something to the internet
+  const string = JSON.stringify(data);
+
+  // send the data stored in string to the client
+  stream.updateInit(string);
+
+  // replaces the res.send - connect to the client
+  stream.init(req, res);
+});
+
+// rather than repeating code
+function send(data) {
+  const string = JSON.stringify(data);
+  stream.send(string);
+}
+
+// create a new room
+app.post('/room', (req, res, next) => {
+  const { name } = req.body;
+
+  send(name);
+  // add dynamic property to an object
+  messages[name] = [];
+
+  streams[name] = new See();
+
+  res.send(name);
+});
+
+const messages = {};
+// {
+//   name: [hi, hello]
+//   fun: ['we are here', 'yes']
+// }
+
+app.post('/message/:roomName', (req, res, next) => {
   // get the message out of the body of the req
   const { message } = req.body;
+  const { roomName } = req.params;
 
-  // convert message to json string
+  // one of those obj commented under messages object
+  const room = messages[roomName];
+
+  room.push(message);
+
+  const stream = streams[roomName];
+
   const string = JSON.stringify(message);
 
-  // send data over the stream
   stream.send(string);
 
-  // push it to array messages
-  messages.push(message);
   res.send(message);
 });
 
